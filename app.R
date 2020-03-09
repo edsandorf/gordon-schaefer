647a7c432804fbfdbc9e90fd398b44e0f6ff6df1
edsandorf <edsandorf@gmail.com>
2020-03-09 17:00:51 +0000

    Working graphical solution in effort space

--- app.R
+++ app.R
@@ -1,13 +1,39 @@
 # Load packages ----
-pkgs <- c("shiny", "shinyWidgets")
+pkgs <- c("shiny", "shinyjs", "shinyWidgets", "ggplot2")
 invisible(lapply(pkgs, require, character.only = TRUE))
 
 # Global functions ----
-# The yield-effort ¨curve
+# The yield-effort curve
 yield_effort <- function(K, E, q, r) {
   q * K * E * (1 - ((q / r) * E))
 }
 
+# Add segment and label to the graphical solution
+add_segment_and_label <- function(x, x_int, y_end, color, label) {
+  x + 
+    geom_segment(aes(x = x_int, y = 0, xend = x_int, yend = y_end),
+                 linetype = "dashed", color = color, size = 1.5) +
+    geom_label(aes(x = x_int, y = y_end, label = label))
+}
+
+# Global values ----
+# Set the global parameters
+default <- list()
+default$r <- 0.2
+default$K <- 1000
+default$q <- 0.002
+default$p <- 20
+default$c <- 10
+default$th <- 0
+default$te <- 0
+
+color <- list()
+color$default_revenue <- "blue"
+color$default_cost <- "red"
+color$default_solution <- "grey"
+color$revenue <- "magenta"
+color$cost <- "green"
+color$solution <- "yellow"
 
 # UI ----
 ui <- fluidPage(
@@ -30,41 +56,33 @@ ui <- fluidPage(
     sidebarLayout(
       sidebarPanel(
         class = "sidebar-panel",
-        width = 1,
-        # Set a dropdown menu for the inputs
-        shinyWidgets::dropdownButton(
-          tags$h3("Parameter values"),
-          # Set the inputs
-          numericInput(inputId = "r", label = "Intrinsic growth rate", value = 0.2),
-          numericInput(inputId = "K", label = "Carrying capacity of the stock", value = 1000),
-          numericInput(inputId = "q", label = "Catchability coefficient", value = 0.002),
-          numericInput(inputId = "c", label = "Cost per unit effort", value = 10),
-          numericInput(inputId = "p", label = "Price per unit harvest", value = 20),
-          numericInput(inputId = "th", label = "Tax per unit harvest", value = 0),
-          numericInput(inputId = "te", label = "Tax per unit effort", value = 0),
-          
-          # Define the reset button
-          actionButton(inputId = "reset", label = "Reset"),
-          # Define the options of the dropdown button
-          circle = TRUE, status = "danger", icon = icon("gear"), width = "300px",
-          tooltip = tooltipOptions(title = "Click to see and change parameter values")
+        width = 4,
+        # Set the inputs
+        div(
+          id = "parameters",
+          sliderInput("r", "Intrinsic growth rate", min = 0.1, max = 0.3, step = 0.01, value = default$r),
+          sliderInput("K", "Carrying capacity of the stock", min = 250, max = 1750, step = 50, value = default$K),
+          sliderInput("q", "Catchability coefficient", min = 0.001, max = 0.003, step = 0.0001, value = default$q),
+          sliderInput("p", "Price per unit harvest", min = 0, max = 30, step = 0.5, value = default$p),
+          sliderInput("c", "Cost per unit effort", min = 0, max = 30, step = 0.5, value = default$c),
+          numericInput("th", "Tax per unit harvest", value = default$th),
+          numericInput("te", "Tax per unit effort", value = default$te),
+          # Add row of checkboxes for OA, MEA, MSY, when checked render vertical line
+          checkboxGroupInput("solutions", "Show solution graphically", c("MEY", "MSY", "OA"), inline = TRUE)
         ),
-        # Add row of checkboxes for OA, MEA, MSY, when checked render vertical line
-        
+        actionButton("reset", "Reset values to default")
       ),
       mainPanel(
         tabsetPanel(
           tabPanel(
             "Description",
-            uiOutput("description")
+            div(
+              h2("This is a description")
+            )
           ),
           tabPanel(
-            "Graphical solution",
-            plotOutput("graphical")
-          ),
-          tabPanel(
-            "Numerical solution",
-            uiOutput("numerical")
+            "Model",
+            uiOutput("ui_model")
           )
         )
       )
@@ -74,39 +92,145 @@ ui <- fluidPage(
 
 # Server ----
 server <- function(input, output, session) {
-  
-  # 
-  # total_revenue <- input$p * yield_effort(input$K, effort, input$q, input$r)
-  
-  
-  
-  # Get the input values
-  # input$r
-  # input$K
-  # input$q
-  # input$c
-  # input$p
-  # input$th
-  # input$te
-  
-  # Description ----
-  description <- reactive(
+  # Reset values when the "reset" button is clicked ----
+  observeEvent(
     {
+      input[["reset"]]
+    },
+    {
+      reset("parameters")
+    }
+  )
+  
+  # Non-reactive calculations ----
+  effort <- 1:100
+  default_revenue <- (default$p + default$th) * yield_effort(default$K, effort, default$q, default$r)
+  default_cost <- default$c * effort
+  
+  revenue <- default_revenue
+  cost <- default_cost
+  revenue_changed <- cost_changed <- FALSE
+  
+  
+  # Render effort space graphical solution ----
+  output[["ui_graphical"]] <- renderPlot(
+    {
+      # Define the default plot
+      gs_plot <- ggplot() +
+        geom_line(aes(x = effort, y = default_revenue), color = color$default_revenue, size = 1.5) +
+        geom_line(aes(x = effort, y = default_cost), color = color$default_cost, size = 1.5) +
+        xlab("Effort") +
+        scale_x_continuous(breaks = seq(0, 100, 10), labels = seq(0, 100, 10)) +
+        ylab("Total revenue / Total cost") +
+        scale_y_continuous(breaks = seq(0, 2500, 100), labels = seq(0, 2500, 100), limits = c(0, NA)) +
+        theme(
+          panel.border = element_blank(), 
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(size = .5, linetype = "solid", color = "grey"),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"),
+          strip.text = element_text(size = 11),
+          axis.text = element_text(size = 11),
+          axis.title = element_text(size = 11),
+          legend.text = element_text(size = 11)
+          )
+      
+      # Check whether we have changes
+      if (input$r != default$r || input$K != default$K || input$q != default$q || input$p != default$p || input$th != default$th) {
+        revenue_changed <- TRUE
+        revenue <- (input$p + input$th) * yield_effort(input$K, effort, input$q, input$r)
+        gs_plot <- gs_plot +
+          geom_line(aes(x = effort, y = revenue), color = color$revenue, size = 1.5) 
+      }
+      
+      if (input$c != default$c || input$te != default$te) {
+        cost_changed <- TRUE
+        cost <- (input$c + input$te) * effort
+        gs_plot <- gs_plot +
+          geom_line(aes(x = effort, y = cost), color = color$cost, size = 1.5)
+      }
+      
+      # MEY, MSY, OA ----
+      if ("MEY" %in% input[["solutions"]]) {
+        profit_d <- default_revenue - default_cost
+        mey_d <- which(max(profit_d) == profit_d)[1]
+        gs_plot <- add_segment_and_label(gs_plot, mey_d, default_revenue[mey_d], color$default_solution, "MEY")
+        
+        # Check if parameters have changed ----
+        if (revenue_changed || cost_changed) {
+          profit <- revenue - cost
+          mey <- which(max(profit) == profit)[1]
+          gs_plot <- add_segment_and_label(gs_plot, mey, revenue[mey], color$default_solution, "MEY*")
+        }
+      }
+      
+      if ("MSY" %in% input[["solutions"]]) {
+        msy <- which(max(default_revenue) == default_revenue)[1]
+        gs_plot <- add_segment_and_label(gs_plot, msy, default_revenue[msy], color$default_solution, "MSY")
+        
+        # Check if parameters have changed ----
+        if (revenue_changed || cost_changed) {
+          msy_d <- which(max(revenue) == revenue)[1]
+          gs_plot <- add_segment_and_label(gs_plot, msy_d, revenue[msy_d], color$default_solution, "MSY*")
+        }
+      }
+      
+      if ("OA" %in% input[["solutions"]]) {
+        oa <- which(default_revenue == default_cost)[1]
+        gs_plot <- add_segment_and_label(gs_plot, oa, default_revenue[oa], color$default_solution, "OA")
+        
+        # Check if parameters have changed ----
+        if (revenue_changed || cost_changed) {
+          oa_d <- which(revenue == cost)[1]
+          # gs_plot <- add_segment_and_label(gs_plot, oa_d, revenue[oa_d], color$default_solution, "OA*")
+          gs_plot <- add_segment_and_label(gs_plot, oa_d, revenue[oa_d], color$default_solution, "OA*")
+        }
+      }
+      
+      # Render plot ----
+      gs_plot
+    }
+  )
+  
+  # Numerical results ----
+  ui_numerical <- reactive(
+    {
+      observe(
+        {
+          output[["check_answer"]] <- renderPrint(
+            {
+              str(input[["solutions"]])
+            }
+          )
+        }
+      )
+      return(
+        tags$div(
+          verbatimTextOutput("check_answer")
+        )
+      )
+      
       
     }
   )
   
-  output[["description"]] <- renderUI(
+  # UI Graphical and Numerical  ----
+  output[["ui_model"]] <- renderUI(
     {
-      description()
+      return(
+        withTags(
+          div(
+            h1("Graphical solution in effort space"),
+            plotOutput("ui_graphical"),
+            h1("Graphical solution in stock space"),
+            h1("Numerical solution"),
+            ui_numerical()
+          )
+        )
+      )
     }
   )
-  
-  # Graphical output ----
-  
-  
-  # Numerical output ----
-  
   
 }
 
